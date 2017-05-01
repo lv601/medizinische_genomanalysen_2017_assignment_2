47969cdaad8ad881793a30ca6169c537d85f6976
lv601 <martin.gollobich.78@gmail.com>
2017-05-01 12:55:10 +0200

    Update and rename assignment2.py to assignment2_v2.py

--- assignment2_v2.py
+++ assignment2_v2.py
@@ -2,53 +2,36 @@
 # vim: set fileencoding=UTF-8 :
 
 import vcf
+import statistics
+import traceback
 
 __author__ = 'mgollobich'
-
 
 class Assignment2:
     
     def __init__(self):
         ## Check if pyvcf is installed
         print("PyVCF version: %s" % vcf.VERSION, "\n")
-
-
         self.filename = 'AmpliseqExome.20141120.NA24385.vcf'
         self.file = vcf.Reader(open(self.filename, 'r'))
-        #rec = next(self.file)
-        
-        #Used Reference: https://github.com/jamescasbon/PyVCF/blob/master/README.rst
-        
-
+        self.records = [record for record in self.file]
 
     def get_average_quality_of_son(self):
         '''
         Get the average PHRED quality of all variants
         :return: 
-        '''    
-        print("Calculating avg record.QUAL")
-        self.file = vcf.Reader(open(self.filename, 'r'))
-        quality = 0
-        n = 0
-        for record in self.file:
-            quality += record.QUAL
-            n += 1
-        avg = quality/n
-        return avg, n
+        '''
+        mean = statistics.mean(record.QUAL for record in self.records)
+        n = self.get_total_number_of_variants_of_son()
+        return mean, n
 
-        
-        
     def get_total_number_of_variants_of_son(self):
         '''
         Get the total number of variants
         :return: total number of variants
         '''
-        number = 0
-        for record in self.file:
-            number += 1
-        return number
-    
-    
+        return sum(1 for _ in self.records)
+
     def get_variant_caller_of_vcf(self):
         '''
         Return the variant caller name
@@ -56,14 +39,13 @@ class Assignment2:
         '''
         vcaller = self.file.metadata['source'][0]
         return vcaller
-        
-        
+
     def get_human_reference_version(self):
         '''
         Return the genome reference version
         :return: 
         '''
-        self.file = vcf.Reader(open(self.filename, 'r'))
+        # self.file = vcf.Reader(open(self.filename, 'r'))
         ## Jede Zeile des Headers (_header_lines) wird durchsucht
         for line in self.file._header_lines:
             ## wenn die Zeile mit ##reference beginnt, enthält sie die Info über den Caller
@@ -76,46 +58,43 @@ class Assignment2:
                 version = version[0].split("/")
                 ## beim Split wird eine Liste zurückgegeben deshalb verwende ich den 0. (1.) Eintrag.
         return version[7]
-        
-        
+
+    def get_attribute_count(self, attribute):
+        return sum(1 for record in self.records if getattr(record, attribute))
+
     def get_number_of_indels(self):
         '''
         Return the number of identified INDELs 
         :return: 
         '''
-        reader = vcf.Reader(open(self.filename, 'r'))
-        indel = 0
-        for record in reader:
-            if record.is_indel:
-                indel += 1
-        return indel
+        return self.get_attribute_count("is_indel")
 
     def get_number_of_snvs(self):
         '''
         Return the number of SNVs
         :return: 
         '''
-        reader = vcf.Reader(open(self.filename, 'r'))
-        snp = 0
-        for record in reader:
-            if record.is_snp:
-                snp += 1
-        return snp
+        return self.get_attribute_count("is_snp")
+
     def get_number_of_heterozygous_variants(self):
         '''
         Return the number of heterozygous variants
         :return: 
         '''
-        reader = vcf.Reader(open(self.filename, 'r'))
-        n_het = 0
-        for record in reader:
-            if record.num_het:
-                n_het += 1
-        return n_het
+        return self.get_attribute_count("num_het")
         
     def print_summary(self):
+        print("TEST --------------------------")
+        average, n = self.get_average_quality_of_son()
+        print("Average: {} // N: {}".format(average, n))
+
+        average = self.get_average_quality_of_son()[0]
+        print("Average: {}".format(average, n))
+        print("TEST END ----------------------", end="\n\n")
+
         print(self.get_average_quality_of_son()," - Average quality of SON, Number of Variants ")
-        print("The variant caller of vcf is: ",self.get_variant_caller_of_vcf(),"\n")
+        print("The variant caller of vcf is: ", self.get_variant_caller_of_vcf(),"\n")
+
         print('{:<8}'.format(self.get_number_of_indels()), " - Number of Indels")
         print('{:<8}'.format(self.get_number_of_snvs()), " - Number of SNPs")
         print('{:<8}'.format(self.get_number_of_heterozygous_variants())," - Number of Heterozygous Variants")   
@@ -125,7 +104,10 @@ class Assignment2:
 if __name__ == '__main__':
     print("Assignment 2: ",__author__)
     assignment1 = Assignment2()
-    assignment1.print_summary()
+
+    try:
+        assignment1.print_summary()
+    except Exception as e:
+        traceback.print_exc()
 
 #Used Reference: https://github.com/jamescasbon/PyVCF/blob/master/README.rst
-
